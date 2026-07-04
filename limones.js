b7e05e41ef21b91a41ea622f9fb6102d0d54357f
Holger2005 <ordonezholger43@gmail.com>
2026-07-03 21:56:19 -0500

    subir velocidad con la que cae el limón

--- limones.js
+++ limones.js
@@ -75,6 +75,13 @@ function detectarAtrapado(){
         aparecerLimon();
         puntaje++;
         mostrarEnSpan("txtPuntaje", puntaje);
+        if (puntaje == 3){
+            velocidadCaida = 150;
+        } else if (puntaje == 6){
+            velocidadCaida = 100;
+        } else if (puntaje == 10){
+            alert("Con todos estos limones, puedes hacer una limonada");
+        }
     }
 }
 
